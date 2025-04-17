372757d2556ff63bb9eb04d402a8b699474f5f95
rjanciro <rjanciro@gmail.com>
2025-04-17 20:15:42 +0800

    added links for render'

--- server.js
+++ server.js
@@ -19,7 +19,13 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5000', 'http://localhost:5173', 'http://127.0.0.1:5000', 'http://127.0.0.1:5173'],
+  origin: [
+    'http://localhost:5000', 
+    'http://localhost:5173', 
+    'http://127.0.0.1:5000', 
+    'http://127.0.0.1:5173',
+    'https://homeserve.host'
+  ],
   credentials: true,
   exposedHeaders: ['Content-Disposition']
 }));
